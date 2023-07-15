acc77eb4576518bcec200bf1bb2c43743fd01493
Irfan Irwanuddin <irfanirwanuddin@gmail.com>
2023-07-15 16:59:01 +0800

    loop program introduced

--- ParallelLoop/Program.cs
+++ ParallelLoop/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+
+namespace ParallelLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ints = Enumerable.Range(0, 10);
+
+            var sw = new Stopwatch();
+
+            sw.Start();
+            Parallel.For(0, ints.Count(), i =>
+            {
+                Console.WriteLine("i = " + i + "\tprocId = " + Thread.GetCurrentProcessorId().ToString());
+                Thread.Sleep(1000);
+            });
+
+            for (int i = 0; i < ints.Count(); i++)
+            {
+                Console.WriteLine("i = " + i);
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
